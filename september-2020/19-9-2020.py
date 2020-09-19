1a9f8f8e09af991776bd3ae5cc67132bc2e30aa8
dinesh <dineshkrishnareddy@gmail.com>
2020-09-19 19:53:44 +0400

    daily challenge

---
+++ september-2020/19-9-2020.py
@@ -0,0 +1,24 @@
+"""
+Sequential Digits
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+"""
+
+
+class Solution:
+    def digits_count(self, num):
+        return len(str(num))
+
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        low_count = self.digits_count(low)
+        high_count = self.digits_count(high)
+
+        result = []
+        for count in range(low_count, high_count + 1):
+            for start in range(1, 10 - count + 1):
+                num = int(''.join([str(x) for x in range(start, start + count)]))
+                if num < low:
+                    continue
+                elif num > high:
+                    break
+                result.append(num)
+        return result
