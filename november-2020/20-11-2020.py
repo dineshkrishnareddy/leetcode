041a3868e13ffe02be07c2884d30a9106c04e974
dinesh <dineshkrishnareddy@gmail.com>
2020-11-21 09:50:40 +0400

    daily challenge

---
+++ november-2020/20-11-2020.py
@@ -0,0 +1,24 @@
+"""
+Decode String
+"""
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        # return target in nums
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            m = left + right >> 1
+            if nums[m] == target: return True
+
+            while nums[left] == nums[m] == nums[right]:
+                left += 1
+                right -= 1
+                if left > right: return False
+
+            if (target < nums[left], target) < (nums[m] < nums[left], nums[m]):
+                right = m - 1
+            else:
+                left = m + 1
+
+        return False
