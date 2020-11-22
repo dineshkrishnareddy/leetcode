1785c848e040fe60fed25ae320f4c2edcb42de9c
dinesh <dineshkrishnareddy@gmail.com>
2020-11-22 08:21:42 +0400

    daily challenge

---
+++ november-2020/21-11-2020.py
@@ -0,0 +1,23 @@
+"""
+Numbers At Most N Given Digit Set
+https://leetcode.com/problems/numbers-at-most-n-given-digit-set/
+"""
+
+
+class Solution:
+    def atMostNGivenDigitSet(self, D, N):
+        S = str(N)
+        K = len(S)
+        dp = [0] * K + [1]
+        # dp[i] = total number of valid integers if N was "N[i:]"
+
+        for i in range(K - 1, -1, -1):
+            # Compute dp[i]
+
+            for d in D:
+                if d < S[i]:
+                    dp[i] += len(D) ** (K - i - 1)
+                elif d == S[i]:
+                    dp[i] += dp[i + 1]
+
+        return dp[0] + sum(len(D) ** i for i in range(1, K))
