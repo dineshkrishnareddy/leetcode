f623a702344dc76714307abf46fad5347ac206f2
dinesh <dineshkrishnareddy@gmail.com>
2020-12-21 20:06:01 +0400

    daily challenge

---
+++ december-2020/20-12-2020.py
@@ -0,0 +1,15 @@
+"""
+Smallest Range II
+https://leetcode.com/problems/smallest-range-ii/
+"""
+
+
+class Solution(object):
+    def smallestRangeII(self, A, K):
+        A.sort()
+        mi, ma = A[0], A[-1]
+        ans = ma - mi
+        for i in range(len(A) - 1):
+            a, b = A[i], A[i+1]
+            ans = min(ans, max(ma-K, a+K) - min(mi+K, b-K))
+        return ans
