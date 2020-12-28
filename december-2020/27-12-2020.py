48b8108579d31400fa68fc61f0e11a644c7d967b
dinesh <dineshkrishnareddy@gmail.com>
2020-12-28 20:49:24 +0400

    daily challenge

---
+++ december-2020/27-12-2020.py
@@ -0,0 +1,15 @@
+"""
+Reach a Number
+https://leetcode.com/problems/reach-a-number/
+"""
+
+
+class Solution(object):
+  def reachNumber(self, target):
+    target = abs(target)
+    k = 0
+    while target > 0:
+      k += 1
+      target -= k
+
+    return k if target % 2 == 0 else k + 1 + k % 2
