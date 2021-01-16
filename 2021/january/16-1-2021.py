754bcb9f901291d1cd032ad0d54e2475d795705c
dinesh <dineshkrishnareddy@gmail.com>
2021-01-16 18:12:01 +0400

    daily challenge

---
+++ 2021/january/16-1-2021.py
@@ -0,0 +1,22 @@
+"""
+Kth Largest Element in an Array
+https://leetcode.com/problems/kth-largest-element-in-an-array/
+"""
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        if not nums: return
+        pivot = random.choice(nums)
+        left = [x for x in nums if x > pivot]
+        mid = [x for x in nums if x == pivot]
+        right = [x for x in nums if x < pivot]
+
+        L, M = len(left), len(mid)
+
+        if k <= L:
+            return self.findKthLargest(left, k)
+        elif k > L + M:
+            return self.findKthLargest(right, k - L - M)
+        else:
+            return mid[0]
