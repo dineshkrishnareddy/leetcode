2aeeb29511a648e83c8f63405e573170b7698624
dinesh <dineshkrishnareddy@gmail.com>
2021-01-27 19:38:02 +0400

    daily challenge

---
+++ 2021/january/27-1-2021.py
@@ -0,0 +1,9 @@
+"""
+Concatenation of Consecutive Binary Numbers
+https://leetcode.com/problems/concatenation-of-consecutive-binary-numbers/d
+"""
+
+
+class Solution:
+    def concatenatedBinary(self, n: int) -> int:
+        return reduce(lambda s, i: (s << i.bit_length() | i) % 1000000007, range(1, n + 1))
