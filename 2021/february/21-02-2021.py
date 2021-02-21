64cb35ccf74dfd2c654856d833e10a3790b34818
dinesh <dineshkrishnareddy@gmail.com>
2021-02-21 17:44:08 +0530

    daily challenge

---
+++ 2021/february/21-02-2021.py
@@ -0,0 +1,17 @@
+"""
+Broken Calculator
+https://leetcode.com/problems/broken-calculator/
+"""
+
+
+class Solution:
+    def brokenCalc(self, X: int, Y: int) -> int:
+        ans = 0
+        while Y > X:
+            ans += 1
+            if Y % 2:
+                Y += 1
+            else:
+                Y /= 2
+
+        return ans + X - Y
