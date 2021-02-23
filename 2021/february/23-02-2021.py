3416be19404054e0239c8db78dde60dcabba859d
dinesh <dineshkrishnareddy@gmail.com>
2021-02-23 18:10:12 +0530

    daily challenge

---
+++ 2021/february/23-02-2021.py
@@ -0,0 +1,17 @@
+"""
+Search a 2D Matrix II
+https://leetcode.com/problems/search-a-2d-matrix-ii/
+"""
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        row, col, num_row = 0, len(matrix[0]) - 1, len(matrix)
+        while col >= 0 and row < num_row:
+            if matrix[row][col] > target:
+                col -= 1
+            elif matrix[row][col] < target:
+                row += 1
+            else:
+                return True
+        return False
