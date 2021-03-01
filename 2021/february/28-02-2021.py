1c4d75d6da893d24b2bbcde7c4ce1b0388f056c0
dinesh <dineshkrishnareddy@gmail.com>
2021-03-01 09:03:44 +0530

    daily challenge

--- 2021/february/28-02-2021.py
+++ 2021/february/28-02-2021.py
@@ -1,29 +1,29 @@
 """
-Score of Parentheses
-https://leetcode.com/problems/score-of-parentheses/
+Maximum Frequency Stack
+https://leetcode.com/problems/maximum-frequency-stack/
 """
 
-
-class Solution:
-    def scoreOfParentheses(self, S: str) -> int:
-        stack = [0]
-        for char in S:
-            if char == '(':
-                stack.append(0)
-            else:
-                val = stack.pop()
-                stack[-1] += max(2 * val, 1)
-
-        return stack.pop()
+import collections
 
 
-# string = "()"
-# print(Solution().scoreOfParentheses(string))
-# string = "(())"
-# print(Solution().scoreOfParentheses(string))
-string = "((()))"
-print(Solution().scoreOfParentheses(string))
-# string = "((()()))"
-# print(Solution().scoreOfParentheses(string))
-# string = "((())"
-# print(Solution().scoreOfParentheses(string))
+class FreqStack(object):
+
+    def __init__(self):
+        self.freq = collections.Counter()
+        self.group = collections.defaultdict(list)
+        self.maxfreq = 0
+
+    def push(self, x):
+        f = self.freq[x] + 1
+        self.freq[x] = f
+        if f > self.maxfreq:
+            self.maxfreq = f
+        self.group[f].append(x)
+
+    def pop(self):
+        x = self.group[self.maxfreq].pop()
+        self.freq[x] -= 1
+        if not self.group[self.maxfreq]:
+            self.maxfreq -= 1
+
+        return x
