a435b20338184bcf4a3b4dc43f8fdfe4db364311
dinesh <dineshkrishnareddy@gmail.com>
2021-03-13 12:08:27 +0530

    daily challenge

---
+++ 2021/march/12-3-2021.py
@@ -0,0 +1,13 @@
+"""
+Coin Change
+https://leetcode.com/problems/coin-change/solution/
+"""
+
+
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        got = {s[i - k : i] for i in range(k, len(s) + 1)}
+        return len(got) == 1 << k
+
+
+print(Solution().hasAllCodes("00110110", 2))
