019a0366ac371e40978ab3543f003c8b25d1c514
dinesh <dineshkrishnareddy@gmail.com>
2021-04-13 08:37:19 +0400

    daily challenge

---
+++ 2021/april/12-4-2021.py
@@ -0,0 +1,16 @@
+"""
+Beautiful Arrangement II
+https://leetcode.com/problems/beautiful-arrangement-ii/
+"""
+
+
+class Solution(object):
+    def constructArray(self, n, k):
+        ans = list(range(1, n - k))
+        for i in range(k+1):
+            if i % 2 == 0:
+                ans.append(n-k + i//2)
+            else:
+                ans.append(n - i//2)
+
+        return ans
