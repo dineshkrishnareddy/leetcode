8ff2220c327f926de97428f5c4a29b5a4ab8445e
dinesh <dineshkrishnareddy@gmail.com>
2021-04-26 17:56:56 +0400

    daily challenge

---
+++ 2021/april/26-4-2021.py
@@ -0,0 +1,20 @@
+"""
+Furthest Building You Can Reach
+https://leetcode.com/problems/furthest-building-you-can-reach/
+"""
+from heapq import heappop, heappush
+
+
+class Solution:
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        jumps_pq = []
+        for i in range(len(heights) - 1):
+            jump_height = heights[i + 1] - heights[i]
+            if jump_height < 0:
+                continue
+            heappush(jumps_pq, jump_height)
+            if len(jumps_pq) > ladders:
+                bricks -= heappop(jumps_pq)
+            if bricks < 0:
+                return i
+        return len(heights) - 1
