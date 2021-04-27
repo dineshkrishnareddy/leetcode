5ccb48c9dc06e508b7091945157b4475c148d9eb
dinesh <dineshkrishnareddy@gmail.com>
2021-04-27 17:11:34 +0400

    daily challenge

---
+++ 2021/april/27-4-2021.py
@@ -0,0 +1,9 @@
+"""
+Power of Three
+https://leetcode.com/problems/power-of-three/
+"""
+
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        return n > 0 and 1162261467 % n == 0
