90ccae575a57f95f53e5cccfa8a029e2d0bd30e1
dinesh <dineshkrishnareddy@gmail.com>
2021-05-05 08:08:36 +0400

    daily challenge

---
+++ 2021/may/4-5-2021.py
@@ -0,0 +1,15 @@
+"""
+Non-decreasing Array
+https://leetcode.com/problems/non-decreasing-array/
+"""
+
+
+class Solution:
+    def checkPossibility(self, N: List[int]) -> bool:
+        err = 0
+        for i in range(1, len(N)):
+            if N[i] < N[i-1]:
+                if err or (i > 1 and i < len(N) - 1 and N[i-2] > N[i] and N[i+1] < N[i-1]):
+                    return False
+                err = 1
+        return True
