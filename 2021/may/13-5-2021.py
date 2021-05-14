b0b24edf9de9b606899f33cdfabebeccd0a838e1
dinesh <dineshkrishnareddy@gmail.com>
2021-05-14 12:15:45 +0400

    daily challenge

--- 2021/may/13-5-2021.py
+++ 2021/may/13-5-2021.py
@@ -1,23 +1,30 @@
 """
-Count Primes
-https://leetcode.com/problems/count-primes/
+816. Ambiguous Coordinates
+https://leetcode.com/problems/ambiguous-coordinates/
 """
-from math import sqrt
 
 
 class Solution:
-    def countPrimes(self, n: int) -> int:
-        if n <= 2:
-            return 0
+    def ambiguousCoordinates(self, S: str):
+        ans, xPoss = [], []
+        def process(st: str, xy: int):
+            if xy:
+                for x in xPoss:
+                    ans.append("(" + x + ", " + st + ")")
+            else: xPoss.append(st)
+        def parse(st: str, xy: int):
+            if len(st) == 1 or st[0] != "0":
+                process(st, xy)
+            if len(st) > 1 and st[-1] != "0":
+                process(st[:1] + "." + st[1:], xy)
+            if len(st) > 2 and st[0] != "0" and st[-1] != "0":
+                for i in range(2, len(st)):
+                    process(st[:i] + "." + st[i:], xy)
+        for i in range(2, len(S)-1):
+            strs, xPoss = [S[1:i], S[i:-1]], []
+            for j in range(2):
+                if xPoss or not j: parse(strs[j], j)
+        return ans
 
-        numbers = {}
-        for p in range(2, int(sqrt(n)) + 1):
-            if p not in numbers:
-                for multiple in range(p * p, n, p):
-                    numbers[multiple] = 1
 
-        # Exclude "1" and the number "n" itself.
-        return n - len(numbers) - 2
-
-
-print(Solution().countPrimes(100))
+print(Solution().ambiguousCoordinates('(00011)'))
