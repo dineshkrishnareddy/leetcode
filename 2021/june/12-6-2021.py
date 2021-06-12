df6cc47f641d800624b85b1274c78b0a066574a0
dinesh <dineshkrishnareddy@gmail.com>
2021-06-12 20:31:23 +0400

    daily challenge

---
+++ 2021/june/12-6-2021.py
@@ -0,0 +1,17 @@
+"""
+871. Minimum Number of Refueling Stops
+https://leetcode.com/problems/minimum-number-of-refueling-stops/
+"""
+
+
+class Solution(object):
+    def minRefuelStops(self, target, startFuel, stations):
+        dp = [startFuel] + [0] * len(stations)
+        for i, (location, capacity) in enumerate(stations):
+            for t in range(i, -1, -1):
+                if dp[t] >= location:
+                    dp[t+1] = max(dp[t+1], dp[t] + capacity)
+
+        for i, d in enumerate(dp):
+            if d >= target: return i
+        return -1
