e49b0126664c10d86a911e9f09bdc547a48e7e33
dinesh <dineshkrishnareddy@gmail.com>
2021-07-13 15:22:09 +0400

    daily challenge

---
+++ 2021/july/13-07-2021.py
@@ -0,0 +1,17 @@
+"""
+162. Find Peak Element
+https://leetcode.com/problems/find-peak-element/
+"""
+
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        low=0
+        high=len(nums)-1
+        while low<high:
+            mid=(low+high)//2
+            if nums[mid]>nums[mid+1]:
+                high=mid
+            else:
+                low=mid+1
+        return high
